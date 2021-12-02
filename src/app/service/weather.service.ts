11c113e3316cd4cf303dc0c6fb18fdbc252448da
amirshafri96 <mohamad.shafri.amir@gmail.com>
2021-12-02 13:46:35 +0800

    add weather endpoint

---
+++ src/app/service/weather.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Position } from '@angular/compiler';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { }
+
+  getData(data): Observable<string> {
+    let url = environment.serviceip + "weather/getForecastForCity?cityName=" + data;
+    return this.http.get<string>(url);
+  }
+}
